d2e6edf9e425ef16714f033684e4217f314cc57e
Jason Dreyzehner <jason@dreyzehner.com>
2017-06-27 19:59:11 -0400

    chore(init): typescript-starter -> dc-pool

--- src/lib/hash.ts
+++ src/lib/hash.ts
@@ -5,7 +5,7 @@ import { createHash } from 'crypto'
  *
  * ### Example (es imports)
  * ```js
- * import { sha256 } from 'typescript-starter'
+ * import { sha256 } from 'dc-pool'
  * sha256('test')
  * // => '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
  * ```
